4629fb9213824a23943831ca3c57ff70caba7b32
fcn-mq <qing.mei@fenrir-inc.com.cn>
2017-04-27 14:32:05 +0800

    code submit

--- library/src/main/java/com/mei_husky/library/view/QRCoverView.java
+++ library/src/main/java/com/mei_husky/library/view/QRCoverView.java
@@ -7,13 +7,11 @@ import android.graphics.RectF;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.TypedValue;
+import android.util.Log;
 import android.view.View;
 
 import com.mei_husky.library.R;
 import com.mei_husky.library.util.DpUtils;
-
-import static android.util.TypedValue.COMPLEX_UNIT_DIP;
 
 /**
  * 这个View覆盖于QRCodeScannerView之上，用于UI的绘制，比如扫描框，扫描线,扫描框内外的背景颜色以及透明度等等。
@@ -26,7 +24,7 @@ public class QRCoverView extends View {
     //扫描框的宽和高
     private float scannerW;
     private float scannerH;
-    //扫描框距离屏幕左上角的距离
+    //扫描框的上边距和左边距
     private float top;
     private float left;
     //扫描边框的长度和厚度
@@ -48,29 +46,50 @@ public class QRCoverView extends View {
 
         DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
 
+        //默认大小为180dp
         left = (displayMetrics.widthPixels - DpUtils.dip2px(context, 180)) / 2;
         top = (displayMetrics.heightPixels - DpUtils.dip2px(context, 180)) / 3;
-//    Log.i("tag","屏幕宽高=" + displayMetrics.widthPixels + "*" + displayMetrics.heightPixels );
-//    Log.i("tag","扫描区域 left & top=" + left + "  &  " + top );
-//    Log.i("tag","矩形区域 宽高=" + DpUtils.dip2px(context, 540) + "  *  " + DpUtils.dip2px(context, 540) );
-//    top = TypedValue.applyDimension(COMPLEX_UNIT_DIP, 28, displayMetrics);
-//    left = TypedValue.applyDimension(COMPLEX_UNIT_DIP, 40, displayMetrics);
-        cornerH = TypedValue.applyDimension(COMPLEX_UNIT_DIP, 48, displayMetrics);
-        cornerW = TypedValue.applyDimension(COMPLEX_UNIT_DIP, 10, displayMetrics);
+        //默认扫描框为16dp长，3dp宽
+        cornerH = DpUtils.dip2px(context, 16);
+        cornerW = DpUtils.dip2px(context, 3);
+        //默认背景色和扫描边框颜色
         paint.setColor(ContextCompat.getColor(getContext(), R.color.cover_bg));
         cornerPaint.setColor(ContextCompat.getColor(getContext(), R.color.colorPrimaryDark));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        viewFinderRect = new RectF(left, top, left + DpUtils.dip2px(context, 180), top + DpUtils.dip2px(context, 180));
+        //默认扫描框为180dp
+        if (scannerW == 0f || scannerH == 0f) {
+            viewFinderRect = new RectF(left, top, left + DpUtils.dip2px(context, 180), top + DpUtils.dip2px(context, 180));
+        } else {
+            viewFinderRect = new RectF(left, top, left + scannerW, top + scannerH);
+        }
+        drawScanBackground(canvas, viewFinderRect);
+        drawScanCorner(canvas, viewFinderRect);
+    }
 
+    /**
+     * 绘制扫描框外部背景
+     *
+     * @param canvas
+     * @param viewFinderRect
+     */
+    private void drawScanBackground(Canvas canvas, RectF viewFinderRect) {
         canvas.drawRect(0, 0, viewFinderRect.left, getHeight(), paint);
         canvas.drawRect(viewFinderRect.left, 0, getWidth(), viewFinderRect.top, paint);
         canvas.drawRect(viewFinderRect.right, viewFinderRect.top, getWidth(), getHeight(), paint);
         canvas.drawRect(viewFinderRect.left, viewFinderRect.bottom, viewFinderRect.right,
                 getHeight(), paint);
+    }
 
+    /**
+     * 绘制扫描边框
+     *
+     * @param canvas
+     * @param viewFinderRect
+     */
+    private void drawScanCorner(Canvas canvas, RectF viewFinderRect) {
         //扫描边框在扫描框外部
         if (isCornerOutside) {
             canvas.drawRect(viewFinderRect.left - cornerW, viewFinderRect.top - cornerW,
@@ -95,23 +114,19 @@ public class QRCoverView extends View {
                     viewFinderRect.left + cornerW, viewFinderRect.top + cornerH, cornerPaint);
             canvas.drawRect(viewFinderRect.left + cornerW, viewFinderRect.top,
                     viewFinderRect.left + cornerH, viewFinderRect.top + cornerW, cornerPaint);
-
             canvas.drawRect(viewFinderRect.right - cornerH, viewFinderRect.top,
                     viewFinderRect.right, viewFinderRect.top + cornerW, cornerPaint);
             canvas.drawRect(viewFinderRect.right - cornerW, viewFinderRect.top + cornerW, viewFinderRect.right,
                     viewFinderRect.top + cornerH, cornerPaint);
-
             canvas.drawRect(viewFinderRect.right - cornerW, viewFinderRect.bottom - cornerH,
                     viewFinderRect.right, viewFinderRect.bottom, cornerPaint);
             canvas.drawRect(viewFinderRect.right - cornerH, viewFinderRect.bottom - cornerW,
                     viewFinderRect.right - cornerW, viewFinderRect.bottom, cornerPaint);
-
             canvas.drawRect(viewFinderRect.left, viewFinderRect.bottom - cornerH,
                     viewFinderRect.left + cornerW, viewFinderRect.bottom, cornerPaint);
             canvas.drawRect(viewFinderRect.left + cornerW, viewFinderRect.bottom - cornerW,
                     viewFinderRect.left + cornerH, viewFinderRect.bottom, cornerPaint);
         }
-
     }
 
     /**
@@ -143,11 +158,12 @@ public class QRCoverView extends View {
      */
     public QRCoverView setCoverViewCorner(int cornerH, int cornerW, boolean isDp) {
         if (isDp) {
-            cornerH = DpUtils.dip2px(getContext(), cornerH);
-            cornerW = DpUtils.px2dip(getContext(), cornerW);
+            this.cornerH = DpUtils.dip2px(context, cornerH);
+            this.cornerW = DpUtils.dip2px(context, cornerW);
+        } else {
+            this.cornerH = cornerH;
+            this.cornerW = cornerW;
         }
-        this.cornerH = cornerH;
-        this.cornerW = cornerW;
         return this;
     }
 
@@ -159,20 +175,24 @@ public class QRCoverView extends View {
     }
 
     /**
-     * 设置扫描框的宽高
+     * 设置扫描框的宽高(dp)
      *
      * @param width
      * @param height
      */
     public QRCoverView setCoverViewScanner(int width, int height) {
-        this.scannerH = height;
-        this.scannerW = width;
+        //配置扫描框的宽高
+        this.scannerH = DpUtils.dip2px(context, height);
+        this.scannerW = DpUtils.dip2px(context, width);
+        //重新获取扫描框的左边距和上边距
+        left = (getResources().getDisplayMetrics().widthPixels - scannerW) / 2;
+        top = (getResources().getDisplayMetrics().heightPixels - scannerH) / 3;
         return this;
     }
 
 
     /**
-     * 设置扫描边框的位置，默认扫描边框在扫描框的内部；
+     * 设置扫描边框的位置；
      * 若调用该setCoverViewConnerFace(true)，则扫描边框则会包裹扫描框显示在界面上；
      * 否则默认扫描边框内贴在扫描框内（大多数app默认都是该模式）。
      *
@@ -187,6 +207,11 @@ public class QRCoverView extends View {
      * 重新执行onDraw()
      */
     public void commitUi() {
+        Log.i("tag", "重新绘制...");
+        Log.i("tag", "扫描边框isOutSide=" + isCornerOutside);
+        Log.i("tag", "扫描边框大小（h/w）=" + cornerH + "/" + cornerW);
+        Log.i("tag", "扫描边框颜色...=" + cornerPaint.getColor());
+        Log.i("tag", "扫描框大小（h/w）=" + scannerW + "/" + scannerH);
         invalidate();
     }
 }
--- sample/src/main/java/com/mei_husky/qrcodescannerview/MainActivity.java
+++ sample/src/main/java/com/mei_husky/qrcodescannerview/MainActivity.java
@@ -3,14 +3,17 @@ package com.mei_husky.qrcodescannerview;
 import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.mei_husky.library.view.QRCodeScannerView;
 import com.mei_husky.library.view.QRCoverView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private QRCodeScannerView mScannerView;
     private QRCoverView mCoverView;
+    private Button mBtnTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         mScannerView = (QRCodeScannerView) findViewById(R.id.scanner_view);
         mCoverView = (QRCoverView) findViewById(R.id.cover_view);
+        mBtnTest = (Button) findViewById(R.id.btn_test);
+        mBtnTest.setOnClickListener(this);
 
         mScannerView.setOnQRCodeReadListener(new QRCodeScannerView.OnQRCodeScannerListener() {
             @Override
@@ -44,5 +49,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         mScannerView.stopCamera();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_test:
+                mCoverView.setCoverViewConnerFace(true)         //扫描边框在扫描框外部（默认为里部）
+                        .setCoverViewCorner(40, 6, true)        //设置扫描边框的长度和厚度（单位是否为dp）
+                        .setCoverViewCornerColor(R.color.colorAccent)//扫描边框的颜色
+                        .setCoverViewScanner(220, 220)          //扫描框的宽度和高度
+                        .setCoverViewOutsideColor(R.color.colorPrimary)//修改扫描框外背景色
+                        .commitUi();                            //修改UI
+                break;
+        }
     }
 }
