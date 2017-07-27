1896e4a4287d697f7c4f9a15c3f4b278533189cb
qing.mei <qing.mei@fenrir-inc.com.cn>
2017-07-27 09:27:36 +0800

    code commit

--- library/src/main/java/com/qingmei2/library/util/QRCodeHelper.java
+++ library/src/main/java/com/qingmei2/library/util/QRCodeHelper.java
@@ -1,4 +1,4 @@
-package com.qingmei2.library;
+package com.qingmei2.library.util;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
@@ -14,6 +14,7 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.qingmei2.library.R;
 
 import java.util.HashMap;
 import java.util.Hashtable;
--- library/src/test/java/com/qingmei2/library/QRCodeHelperTest.java
+++ library/src/test/java/com/qingmei2/library/QRCodeHelperTest.java
@@ -1,9 +1,11 @@
 package com.qingmei2.library;
 
+import com.qingmei2.library.util.QRCodeHelper;
+
 /**
  * Created by QingMei on 2017/7/19.
  * desc:test for {@link QRCodeHelper}
  */
 public class QRCodeHelperTest {
-    
+
 }
